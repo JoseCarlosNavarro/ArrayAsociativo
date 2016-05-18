f51246e8130aca8cdcfb86976e6310cf084c0085
JoseCarlosNavarro <josecnavarro@uma.es>
2016-05-18 18:41:26 +0200

    crearNodoVacio

--- test/Tdd_ArrayAsociativo/arrayAsociativoTest.java
+++ test/Tdd_ArrayAsociativo/arrayAsociativoTest.java
@@ -4,11 +4,16 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import arrayAsociativo.Nodo;
+
 public class arrayAsociativoTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void crearNodoVacio() {
+		Nodo nd = new Nodo();
+		assertEquals(nd.clave, "");
+		assertEquals(nd.valor, "");
+		assertEquals(nd.siguiente, null);
 	}
 
 }
