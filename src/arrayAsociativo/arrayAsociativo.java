1978f0f34b93a54e349edd5f050089656bc83b74
JoseCarlosNavarro <josecnavarro@uma.es>
2016-05-18 20:23:29 +0200

    InsertarNuevaEntradaArrayConClaveRepetida.codigo

--- src/arrayAsociativo/arrayAsociativo.java
+++ src/arrayAsociativo/arrayAsociativo.java
@@ -2,21 +2,27 @@ package arrayAsociativo;
 
 public class arrayAsociativo {
 
-	public Nodo primero ;
-	
-	public arrayAsociativo(){
-		primero=null;
+	public Nodo primero;
+
+	public arrayAsociativo() {
+		primero = null;
 	}
 
 	public void put(String clave, String valor) {
-		if(primero==null){
-		primero = new Nodo(clave,valor);
-		}else{
-			Nodo current = primero;
-			while(current.siguiente !=null) current = current.siguiente;
-			current.siguiente= new Nodo(clave,valor);
+		if (primero == null) {
+			primero = new Nodo(clave, valor);
 		}
-		
+		Nodo current = primero;
+
+		while (current.siguiente != null && current.clave != clave)
+			current = current.siguiente;
+
+		if (current.siguiente == null) {
+			current.siguiente = new Nodo(clave, valor);
+		} else {
+			current.valor = valor;
+		}
+
 	}
 
 }
